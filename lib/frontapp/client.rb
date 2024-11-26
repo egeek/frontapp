f6a321d53444a56bbdd5b3bbedc41705b5c6f406
Stephen Houston <stephen.houston@teamfeepay.com>
2024-11-26 23:37:50 +0000

    Merges from PRs

--- lib/frontapp/client.rb
+++ lib/frontapp/client.rb
@@ -142,15 +142,15 @@ module Frontapp
         res << q.map do |k, v|
           case v
           when Symbol, String
-            "q[#{k}]=#{CGI.escapeURIComponent_uri_component(v)}"
+            "q[#{k}]=#{CGI.escapeURIComponent(v)}"
           when Array then
-            v.map { |c| "q[#{k}][]=#{CGI.escapeURIComponent_uri_component(c.to_s)}" }.join("&")
+            v.map { |c| "q[#{k}][]=#{CGI.escapeURIComponent(c.to_s)}" }.join("&")
           else
-            "q[#{k}]=#{CGI.escapeURIComponent_uri_component(v.to_s)}"
+            "q[#{k}]=#{CGI.escapeURIComponent(v.to_s)}"
           end
         end
       end
-      res << params.map {|k,v| "#{k}=#{CGI.escapeURIComponent_uri_component(v.to_s)}"}
+      res << params.map {|k,v| "#{k}=#{CGI.escapeURIComponent(v.to_s)}"}
       res.join("&")
     end
 
